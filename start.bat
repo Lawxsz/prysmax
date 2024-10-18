47d4becc79661ba5b943d2c493feaf9e317a755b
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2024-10-18 01:53:02 -0300

    Actualizar start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,18 @@
 @echo off
 echo Installing required Python libraries...
 
-REM Install the required libraries
-pip install requests shutil uuid wmi psutil pyarmor==7.6.1 pyinstaller glob re platform Pillow discord-webhook pycryptodomex pycryptodome datetime pywin32 tkinter
+REM Install the required libraries, ignoring errors
+pip install requests 2>nul
+pip install wmi 2>nul
+pip install psutil 2>nul
+pip install pyarmor==7.6.1 2>nul
+pip install pyinstaller 2>nul
+pip install Pillow 2>nul
+pip install discord-webhook 2>nul
+pip install pycryptodomex 2>nul
+pip install pycryptodome 2>nul
+pip install pywin32 2>nul
+pip install customtkinter 2>nul
 
 echo All libraries installed. Now running setup.py...
 
